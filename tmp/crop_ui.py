4fee9ab5145d01452e37de973d2cc1fb508d2d82
Andrew Badger <andrewsbadger@gmail.com>
2026-01-15 07:22:54 -0600

    ci: add Pillow dependency for sprite generation

--- tmp/crop_ui.py
+++ tmp/crop_ui.py
@@ -2,4 +2,4 @@ from PIL import Image
 
 src = r"C:\Users\andre\lawchuck\badgey.org\kimbar\vendor\ui\golden_ui_big.png"
 img = Image.open(src)
-img.crop((105, 169, 392 + 1, 262 + 1)).save(r"C:\Users\andre\lawchuck\badgey.org\kimbar\tmp\crop_frame_top.png")
+img.crop((441, 169, 560 + 1, 262 + 1)).save(r"C:\Users\andre\lawchuck\badgey.org\kimbar\tmp\crop_frame_mid_left.png")
