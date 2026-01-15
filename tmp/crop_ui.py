f8f995de62c09369e3b5d013479751c4d25736fa
Andrew Badger <andrewsbadger@gmail.com>
2026-01-15 07:24:54 -0600

    chore: misc tmp files and lockfile

--- tmp/crop_ui.py
+++ tmp/crop_ui.py
@@ -2,4 +2,10 @@ from PIL import Image
 
 src = r"C:\Users\andre\lawchuck\badgey.org\kimbar\vendor\ui\golden_ui_big.png"
 img = Image.open(src)
-img.crop((441, 169, 560 + 1, 262 + 1)).save(r"C:\Users\andre\lawchuck\badgey.org\kimbar\tmp\crop_frame_mid_left.png")
+
+# Attempt crop for top long button
+img.crop((600, 12, 780, 50)).save(r"C:\Users\andre\lawchuck\badgey.org\kimbar\tmp\crop_button_long1.png")
+# Attempt crop for middle long button
+img.crop((600, 52, 780, 90)).save(r"C:\Users\andre\lawchuck\badgey.org\kimbar\tmp\crop_button_long2.png")
+# Attempt crop for short button block (right)
+img.crop((790, 12, 852, 48)).save(r"C:\Users\andre\lawchuck\badgey.org\kimbar\tmp\crop_button_short1.png")
